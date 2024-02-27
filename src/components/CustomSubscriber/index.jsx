5bf3c22dafc3bd030ad7cf5eeae2154cf324b377
Javier Molina <javier.molinasanz@vonage.com>
2024-02-27 10:52:35 +0000

    fixing subscriber video strea,

--- src/components/CustomSubscriber/index.jsx
+++ src/components/CustomSubscriber/index.jsx
@@ -3,19 +3,21 @@ import PushPinIcon from '@mui/icons-material/PushPin';
 
 function CustomSubscriber({ mediaStream }) {
   const videoRef = useRef(null);
-
+  const mediaStreamRef = useRef(null);
   useEffect(() => {
     if (mediaStream && videoRef.current) {
       const videoTrack = mediaStream.getVideoTracks()[0];
       const audioTrack = mediaStream.getAudioTracks()[0];
+      mediaStreamRef.current = mediaStream;
       videoRef.current.srcObject = new MediaStream([videoTrack, audioTrack]);
 
       const handleStreamChange = () => {
         console.log('stream changed');
-        if (mediaStream !== videoRef.current.srcObject) {
+        if (mediaStream !== mediaStreamRef.current) {
           const newAudioTrack = mediaStream.getAudioTracks()[0];
           const newVideoTrack = mediaStream.getVideoTracks()[0];
           videoRef.current.srcObject = new MediaStream([newAudioTrack, newVideoTrack]);
+          mediaStreamRef.current = mediaStream;
         }
       };
 
